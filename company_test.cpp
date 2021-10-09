04555fc8b54afb8579f99955719ceaed22f63e30
Dan1aR <andan.com@mail.ru>
2021-10-09 20:21:29 +0300

    Full test for lib

--- company_test.cpp
+++ company_test.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
+#include <string>
+#include <stdio.h>
 
 #include "company.c"
 
-//Create CMP Array
+
 TEST(COMPANY_TEST, Assert_1) {
     cmp_obj_array cmp = create_array();
     EXPECT_EQ(cmp.size, 0);
@@ -21,11 +23,9 @@ TEST(COMPANY_TEST, Assert_3) {
     cmp_obj_array cmp = create_array();
     char tp[3] = "t1";
     char nm[3] = "n1";
-    add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
-    add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
-    add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
-    add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
-    add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
+
+    for (int i = 0; i < 5; ++i)
+        add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
 
     EXPECT_EQ(cmp.size, 5);
     EXPECT_EQ(cmp.buffer_size, 8);
@@ -40,8 +40,8 @@ TEST(COMPANY_TEST, Assert_4) {
     add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
     add_el(&cmp, tp, 105, nm, 6, 12, 2001, 0, 0);
     add_el(&cmp, tp, 104, nm, 6, 12, 2001, 0, 0);
-    
     int ans[3] = {2, 1, 0};
+
     int mx[3] = {-1, -1, -1};
     find_three_max_counterparty(&cmp, mx);
 
@@ -56,6 +56,30 @@ TEST(COMPANY_TEST, Assert_5) {
     clear_array(&cmp);
     EXPECT_EQ(cmp.arr, nullptr);
 }
+
+/*
+TEST(MAIN_TEST, Asser_6) {
+    ::testing::internal::CaptureStdout();
+
+    cmp_obj_array cmp = create_array();
+    char tp[3] = "t1";
+    char nm[3] = "n1";
+
+    add_el(&cmp, tp, 125, nm, 6, 12, 2001, 0, 0);
+    add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
+    add_el(&cmp, tp, 105, nm, 6, 12, 2001, 0, 0);
+
+    ASSERT_DEATH(interract(2, &cmp), "");
+    std::string capturedStdout = ::testing::internal::GetCapturedStdout().c_str();
+
+    std::string ans = std::string("Самые ценные контрагенты:\n") + \
+        std::string("Имя - Сумма Договора - Сумма Дополнительных соглашений\n") + \
+        std::string(nm) + std::string(" - 125.00 - 0.00 \n") + \
+        std::string(nm) + std::string(" - 123.00 - 0.00 \n") + \
+        std::string(nm) + std::string(" - 105.00 - 0.00 \n"); 
+    EXPECT_STREQ(ans.c_str(), capturedStdout.c_str());
+}
+*/
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
--- main.c
+++ main.c
@@ -50,50 +50,55 @@ void add_info(cmp_obj_array *cmp) {
     //printf("HERE :: %s\n", cmp->arr[cmp->size-1].counterparty_name);
 }
 
+int interract(int user_command, cmp_obj_array *cmp) {
+    if (user_command == 1) {
+        add_info(cmp);
+        //printf("HERE :: %s - %2f - %d\n", cmp.arr[cmp.size-1].counterparty_name, cmp.arr[cmp.size-1].value, cmp.arr[cmp.size-1].day);
+    }
+    else if (user_command == 2) {
+        int *max_idx = (int*)malloc(3*sizeof(int));
+        find_three_max_counterparty(cmp, max_idx);
+        //printf("1) %d %d %d\n", max_idx[0], max_idx[1], max_idx[2]);
+        
+        printf("Самые ценные контрагенты:\n");
+        printf("Имя - Сумма Договора - Сумма Дополнительных соглашений\n");
+        if (max_idx[2] != -1) {
+            printf("%s - %2f - %2f \n", cmp->arr[max_idx[2]].counterparty_name, cmp->arr[max_idx[2]].value, cmp->arr[max_idx[2]].add_agreements_value);
+        }
+        if (max_idx[1] != -1) {
+            //printf("2) %d %d %d\n", max_idx[0], max_idx[1], max_idx[2]);
+            //printf("HERE\n");
+            printf("%s - %2f - %2f \n", cmp->arr[max_idx[1]].counterparty_name, cmp->arr[max_idx[1]].value, cmp->arr[max_idx[1]].add_agreements_value);
+        }
+        if (max_idx[0] != -1) {
+            printf("%s - %2f - %2f \n", cmp->arr[max_idx[0]].counterparty_name, cmp->arr[max_idx[0]].value, cmp->arr[max_idx[0]].add_agreements_value);
+        }
+        
+        free(max_idx);
+    }
+    else if (user_command == 0) {
+        return 0;;
+    }
+    else {
+        printf(">>> Команда не распознана\n");
+    }
+    return 1;
+}
+
 int main () {
-    
+
     cmp_obj_array cmp = create_array();
 
     printf("\t\t\tБаза данных с Информацией о договорах\n");
     int user_command = 1;
+    
     while (user_command) {
         ui();
         scanf("%d", &user_command);
-        if (user_command == 1) {
-            add_info(&cmp);
-            //printf("HERE :: %s - %2f - %d\n", cmp.arr[cmp.size-1].counterparty_name, cmp.arr[cmp.size-1].value, cmp.arr[cmp.size-1].day);
-        }
-        else if (user_command == 2) {
-            int *max_idx = malloc(3*sizeof(int));
-            find_three_max_counterparty(&cmp, max_idx);
-            //printf("1) %d %d %d\n", max_idx[0], max_idx[1], max_idx[2]);
-            
-            printf("Самые ценные контрагенты:\n");
-            printf("Имя - Сумма Договора - Сумма Дополнительных соглашений\n");
-            if (max_idx[2] != -1) {
-                printf("%s - %f - %f \n", cmp.arr[max_idx[2]].counterparty_name, cmp.arr[max_idx[2]].value, cmp.arr[max_idx[2]].add_agreements_value);
-            }
-            if (max_idx[1] != -1) {
-                //printf("2) %d %d %d\n", max_idx[0], max_idx[1], max_idx[2]);
-                //printf("HERE\n");
-                printf("%s - %f - %f \n", cmp.arr[max_idx[1]].counterparty_name, cmp.arr[max_idx[1]].value, cmp.arr[max_idx[1]].add_agreements_value);
-            }
-            if (max_idx[0] != -1) {
-                printf("%s - %f - %f \n", cmp.arr[max_idx[0]].counterparty_name, cmp.arr[max_idx[0]].value, cmp.arr[max_idx[0]].add_agreements_value);
-            }
-            
-            free(max_idx);
-        }
-        else if (user_command == 0) {
+        if (!interract(user_command, &cmp))
             break;
-        }
-        else {
-            printf(">>> Команда не распознана\n");
-        }
     }
 
-    //show_arr(&cmp);
-
     clear_array(&cmp);
-
+       
 }
