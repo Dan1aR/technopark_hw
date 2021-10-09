e72ce48eb8cc4675f6921eadc7cbf91922be96da
Dan1aR <andan.com@mail.ru>
2021-10-10 02:31:55 +0300

    clang-format

--- company.h
+++ company.h
@@ -4,27 +4,27 @@ typedef struct Company Cmp_obj;
 typedef struct cmp_obj_array_struct cmp_obj_array;
 
 struct Company {
-    char *document_type;
-    double value;
-    char *counterparty_name;
-    int day;
-    int mounth;
-    int year;
-    int add_agreements;
-    double add_agreements_value;
+  char *document_type;
+  double value;
+  char *counterparty_name;
+  int day;
+  int mounth;
+  int year;
+  int add_agreements;
+  double add_agreements_value;
 };
 
-Cmp_obj* Company_new(char *, double, char *, int, double);
+Cmp_obj *Company_new(char *, double, char *, int, double);
 
 struct cmp_obj_array_struct {
-    Cmp_obj *arr;
-    int buffer_size;
-    int size;
+  Cmp_obj *arr;
+  int buffer_size;
+  int size;
 };
 
 cmp_obj_array create_array();
-void clear_array(cmp_obj_array*);
-void add_el(cmp_obj_array* ,char*, double, char*, unsigned int, unsigned int, unsigned int, int, double);
-//void show_arr(cmp_obj_array*);
-int* find_three_max_counterparty(cmp_obj_array*, int*);
-
+void clear_array(cmp_obj_array *);
+void add_el(cmp_obj_array *, char *, double, char *, unsigned int, unsigned int,
+            unsigned int, int, double);
+// void show_arr(cmp_obj_array*);
+int *find_three_max_counterparty(cmp_obj_array *, int *);
--- company_test.cpp
+++ company_test.cpp
@@ -1,59 +1,58 @@
-#include <gtest/gtest.h>
 #include <cstring>
+#include <gtest/gtest.h>
 
 #include "company.c"
 
-
 TEST(COMPANY_TEST, Assert_1) {
-    cmp_obj_array cmp = create_array();
-    EXPECT_EQ(cmp.size, 0);
-    EXPECT_EQ(cmp.buffer_size, 4);
+  cmp_obj_array cmp = create_array();
+  EXPECT_EQ(cmp.size, 0);
+  EXPECT_EQ(cmp.buffer_size, 4);
 }
 
 TEST(COMPANY_TEST, Assert_2) {
-    cmp_obj_array cmp = create_array();
-    char tp[3] = "t1";
-    char nm[3] = "n1";
-    add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
-    EXPECT_EQ(cmp.size, 1);
+  cmp_obj_array cmp = create_array();
+  char tp[3] = "t1";
+  char nm[3] = "n1";
+  add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
+  EXPECT_EQ(cmp.size, 1);
 }
 
 TEST(COMPANY_TEST, Assert_3) {
-    cmp_obj_array cmp = create_array();
-    char tp[3] = "t1";
-    char nm[3] = "n1";
+  cmp_obj_array cmp = create_array();
+  char tp[3] = "t1";
+  char nm[3] = "n1";
 
-    for (int i = 0; i < 5; ++i)
-        add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
+  for (int i = 0; i < 5; ++i)
+    add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
 
-    EXPECT_EQ(cmp.size, 5);
-    EXPECT_EQ(cmp.buffer_size, 8);
+  EXPECT_EQ(cmp.size, 5);
+  EXPECT_EQ(cmp.buffer_size, 8);
 }
 
 TEST(COMPANY_TEST, Assert_4) {
-    cmp_obj_array cmp = create_array();
-    char tp[3] = "t1";
-    char nm[3] = "n1";
+  cmp_obj_array cmp = create_array();
+  char tp[3] = "t1";
+  char nm[3] = "n1";
 
-    add_el(&cmp, tp, 125, nm, 6, 12, 2001, 0, 0);
-    add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
-    add_el(&cmp, tp, 105, nm, 6, 12, 2001, 0, 0);
-    add_el(&cmp, tp, 104, nm, 6, 12, 2001, 0, 0);
-    int ans[3] = {2, 1, 0};
+  add_el(&cmp, tp, 125, nm, 6, 12, 2001, 0, 0);
+  add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
+  add_el(&cmp, tp, 105, nm, 6, 12, 2001, 0, 0);
+  add_el(&cmp, tp, 104, nm, 6, 12, 2001, 0, 0);
+  int ans[3] = {2, 1, 0};
 
-    int mx[3] = {-1, -1, -1};
-    find_three_max_counterparty(&cmp, mx);
+  int mx[3] = {-1, -1, -1};
+  find_three_max_counterparty(&cmp, mx);
 
-    EXPECT_TRUE( 0 == std::memcmp( mx, ans, sizeof( ans ) ) );
+  EXPECT_TRUE(0 == std::memcmp(mx, ans, sizeof(ans)));
 }
 
 TEST(COMPANY_TEST, Assert_5) {
-    cmp_obj_array cmp = create_array();
-    char tp[3] = "t1";
-    char nm[3] = "n1";
-    add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
-    clear_array(&cmp);
-    EXPECT_EQ(cmp.arr, nullptr);
+  cmp_obj_array cmp = create_array();
+  char tp[3] = "t1";
+  char nm[3] = "n1";
+  add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
+  clear_array(&cmp);
+  EXPECT_EQ(cmp.arr, nullptr);
 }
 
 /*
@@ -66,17 +65,19 @@ TEST(MAIN_TEST, Asser_6) {
     add_el(&cmp, tp, 123, nm, 6, 12, 2001, 0, 0);
     add_el(&cmp, tp, 105, nm, 6, 12, 2001, 0, 0);
     ASSERT_DEATH(interract(2, &cmp), "");
-    std::string capturedStdout = ::testing::internal::GetCapturedStdout().c_str();
+    std::string capturedStdout =
+::testing::internal::GetCapturedStdout().c_str();
     std::string ans = std::string("Самые ценные контрагенты:\n") + \
-        std::string("Имя - Сумма Договора - Сумма Дополнительных соглашений\n") + \
+        std::string("Имя - Сумма Договора - Сумма Дополнительных соглашений\n")
++ \
         std::string(nm) + std::string(" - 125.00 - 0.00 \n") + \
         std::string(nm) + std::string(" - 123.00 - 0.00 \n") + \
-        std::string(nm) + std::string(" - 105.00 - 0.00 \n"); 
+        std::string(nm) + std::string(" - 105.00 - 0.00 \n");
     EXPECT_STREQ(ans.c_str(), capturedStdout.c_str());
 }
 */
 
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
